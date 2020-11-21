c4b4b11484d4cb964ec255c541607987df4128fc
global_mwy <global_1107201045@qq.com>
2020-11-21 09:51:17 +0800

    Edit AddServerController.java      MCApplication.java      pom.xml files

--- src/main/java/com/lingyi/memcachedclient/MCApplication.java
+++ src/main/java/com/lingyi/memcachedclient/MCApplication.java
@@ -1,12 +1,13 @@
 package com.lingyi.memcachedclient;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class MCApplication extends javafx.application.Application {
+public class MCApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
@@ -15,7 +16,6 @@ public class MCApplication extends javafx.application.Application {
         primaryStage.setTitle("Memcached client");
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 
     public static void main(String[] args) {
--- src/main/java/com/lingyi/memcachedclient/controller/AddServerController.java
+++ src/main/java/com/lingyi/memcachedclient/controller/AddServerController.java
@@ -1,11 +1,11 @@
 package com.lingyi.memcachedclient.controller;
 
 import com.lingyi.memcachedclient.common.NodeUtil;
-import com.lingyi.memcachedclient.common.ProgressFrom;
 import com.lingyi.memcachedclient.pojo.MemcachedConnInfo;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.image.ImageView;
@@ -16,10 +16,8 @@ import net.rubyeye.xmemcached.MemcachedClientBuilder;
 import net.rubyeye.xmemcached.XMemcachedClientBuilder;
 import net.rubyeye.xmemcached.utils.AddrUtil;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class AddServerController implements Initializable {
 
@@ -41,24 +39,31 @@ public class AddServerController implements Initializable {
     }
 
     public void onOk() {
-        TreeItem<String> rootItem = NodeUtil.treeItem;
-        TreeItem<String> treeItem = new TreeItem<>(name.getText());
-        ImageView imageView = new ImageView("/img/icon.png");
-        imageView.setFitWidth(16.0);
-        imageView.setFitHeight(16.0);
-        treeItem.setGraphic(imageView);
-        rootItem.getChildren().add(treeItem);
-        rootItem.setExpanded(true);
         MemcachedConnInfo memcachedConnInfo = new MemcachedConnInfo(name.getText(), host.getText(), Integer.parseInt(port.getText()));
-        NodeUtil.map.put(treeItem, memcachedConnInfo);
         MemcachedClientBuilder memcachedClientBuilder = new XMemcachedClientBuilder(AddrUtil.getAddresses(memcachedConnInfo.getHost() + ":" + memcachedConnInfo.getPort()));
         try {
             MemcachedClient memcachedClient = memcachedClientBuilder.build();
+            if (memcachedClient.getVersions().isEmpty())
+                throw new RuntimeException("");
+            TreeItem<String> rootItem = NodeUtil.treeItem;
+            TreeItem<String> treeItem = new TreeItem<>(name.getText());
+            ImageView imageView = new ImageView("/img/icon.png");
+            imageView.setFitWidth(16.0);
+            imageView.setFitHeight(16.0);
+            treeItem.setGraphic(imageView);
+            rootItem.getChildren().add(treeItem);
+            rootItem.setExpanded(true);
+            NodeUtil.map.put(treeItem, memcachedConnInfo);
             NodeUtil.map1.put(memcachedConnInfo, memcachedClient);
-        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "连接成功", ButtonType.OK);
+            alert.show();
+            onCancel();
+        } catch (Exception e) {
             e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR, "连接失败，具体原因：" + e.getMessage(), ButtonType.OK);
+            alert.show();
         }
-        onCancel();
+
     }
 
     public void onCancel() {
