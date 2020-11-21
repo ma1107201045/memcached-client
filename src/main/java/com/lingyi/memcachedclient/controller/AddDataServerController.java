4d0e916c4db91a316a0c7efd9d332c790a305d7f
global_mwy <global_1107201045@qq.com>
2020-11-21 15:47:01 +0800

    Edit add-data.fxml      AddDataServerController.java

--- src/main/java/com/lingyi/memcachedclient/controller/AddDataServerController.java
+++ src/main/java/com/lingyi/memcachedclient/controller/AddDataServerController.java
@@ -3,6 +3,7 @@ package com.lingyi.memcachedclient.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.awt.*;
 import java.net.URL;
@@ -11,13 +12,22 @@ import java.util.ResourceBundle;
 public class AddDataServerController implements Initializable {
     @FXML
     private AnchorPane anchorPane;
-
+    @FXML
     private TextField key;
-
+    @FXML
     private TextField value;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void onOk() {
+
+    }
+
+    public void onCancel() {
+        Stage stage = (Stage) anchorPane.getScene().getWindow();
+        stage.close();
     }
 }
